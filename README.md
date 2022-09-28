33d8bfbae70796d6ebf446b2e5c23bab2b66860b
valakasgg <109357340+valakasgg@users.noreply.github.com>
2022-09-28 15:45:52 +0100

    missing updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# Simple use of use Reducer Hook
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+See [Demo](valakasgg.github.io/react-counter-useReducer).
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useState, useReducer } from 'react';
 import './App.css';
 
+const ACTIONS = {
+    DECREMENT: 'decrement',
+    INCREMENT: 'increment'
+}
+
+function reducer(state, action) {
+    switch(action.type) {
+        case ACTIONS.DECREMENT:
+            return { count: state.count - 1 }
+        case ACTIONS.INCREMENT:
+            return { count: state.count + 1 }
+        default:
+            return state
+    }
+
+    return { count: state.count + 1 }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Declare new state counter var
+    const [state, dispatch] = useReducer(reducer, {count: 0})
+    const [count, setCount] = useState(0)
+
+    function decrement() {
+        dispatch({ type: ACTIONS.DECREMENT })
+    }
+
+    function increment() {
+        dispatch({ type: ACTIONS.INCREMENT })
+
+    }
+
+    return (
+        <div className="count-wrapper">
+            <button onClick={decrement}>-</button>
+            <button>{state.count}</button>
+            <button onClick={increment}>+</button>
+        </div>
+    );
 }
 
 export default App;
